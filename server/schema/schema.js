4b53424a413a7ccf2f6c164b9b9640c1556dd397
rhmtrz <2249301106@qq.com>
2018-09-20 09:25:05 +0900

    feat: server

---
+++ server/schema/schema.js
@@ -0,0 +1,82 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLID,
+  GraphQLList
+} = graphql;
+
+
+let books = [
+  {name: 'Name of the Wind', genra: 'Fantasy', id: '1', authorId: '1'},
+  {name: 'The Final Empire', genra: 'Fantasy', id: '2', authorId: '2'},
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
+
+let authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
+];
+
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genra: { type: GraphQLString },
+    author: {
+      type: AutherType,
+      resolve(parent, args) {
+        console.log(parent);
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
+  })
+});
+
+const AutherType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    book: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id});
+      }
+    }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return _.find(books, {id: args.id})
+      }
+    },
+    author: {
+      type: AutherType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id})
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
